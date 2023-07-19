2d1ea8c93ea9154a46ed117a9450b9a14b8f4904
vis4rd <alagaesia.vrael@gmail.com>
2023-07-19 02:40:37 +0200

    Experiment with gh-pages package

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import mpa from "vite-plugin-mpa";
-import htmlTemplate from "vite-plugin-html-template";
 
 export default defineConfig({
     plugins: [
@@ -11,30 +10,11 @@ export default defineConfig({
         mpa({
             open: false,
         }),
-        htmlTemplate({
-            pages: {
-                index: {
-                    // entrypoint served under /
-                    template: "./public/index.html",
-                    title: "visard.me",
-                    entry: "./src/pages/home/main.js",
-                },
-                home: {
-                    // entrypoint /home/, same as /
-                    template: "./public/index.html",
-                    title: "visard.me",
-                },
-                about: {
-                    // entrypoiny /about/
-                    template: "./public/index.html",
-                    title: "visard.me / about",
-                },
-            },
-        }),
     ],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
+    base: "/",
 });
