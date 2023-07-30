f565b1cddd0405c7c5bb02c04bd031fa4e8bf0bd
vis4rd <alagaesia.vrael@gmail.com>
2023-07-30 21:08:10 +0200

    Add a funny meme to the 404 page

--- src/pages/404/Error404.vue
+++ src/pages/404/Error404.vue
@@ -1,13 +1,30 @@
 <script setup></script>
 
 <template>
-    <h2>Error 404</h2>
-
-    The page you are looking for does not exist.
     <div>
-        However, you might search for the right address at the
-        <router-link to="/">home page</router-link>.
+        <h1>Page not found (404).</h1>
+
+        <hr />
+        <div class="ohno-img">
+            <img src="@/../assets/ohno-anyway.jpg" alt="Oh no! Anyway" width="300" />
+        </div>
+        <hr />
+
+        <p>
+            The page you are looking for does not exist. However, you might find something by
+            searching through the <router-link to="/">home page</router-link>.
+        </p>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+    .ohno-img {
+        display: flex;
+        flex-direction: column;
+        place-content: center;
+        margin: 0 200px;
+    }
+    img {
+        margin: 1rem;
+    }
+</style>
