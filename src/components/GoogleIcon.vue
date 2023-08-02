080053a383734c9eecd43d33a551068657c6b7e0
vis4rd <alagaesia.vrael@gmail.com>
2023-08-03 00:57:45 +0200

    Add GoogleIcon and IconInfo components

---
+++ src/components/GoogleIcon.vue
@@ -0,0 +1,19 @@
+<script setup>
+    const props = defineProps({
+        name: {
+            type: String,
+            required: true,
+        },
+    });
+</script>
+
+<template>
+    <span class="material-symbols-outlined"> {{ props.name }} </span>
+</template>
+
+<style scoped>
+    span {
+        padding: 0;
+        margin: 0;
+    }
+</style>
---
+++ src/components/IconInfo.vue
@@ -0,0 +1,30 @@
+<script setup>
+    import GoogleIcon from "@/components/GoogleIcon.vue";
+
+    const props = defineProps({
+        icon: {
+            type: String,
+            required: true,
+        },
+    });
+</script>
+
+<template>
+    <span class="icon_info">
+        <GoogleIcon :name="props.icon" />
+        <slot>Icon text</slot>
+    </span>
+</template>
+
+<style scoped>
+    .icon_info {
+        display: flex;
+        flex-direction: row;
+        place-items: flex-start;
+        justify-content: left;
+
+        padding-left: 0.1rem;
+        padding-right: 0.2rem;
+        gap: 0.4rem;
+    }
+</style>
