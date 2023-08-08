0feab92512a36693de778a648f8feb5aacbbcc8b
vis4rd <alagaesia.vrael@gmail.com>
2023-08-08 20:45:24 +0200

    Add BorderWrapper component

---
+++ src/components/BorderWrapper.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+    <div class="border_wrapper">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+    .border_wrapper {
+        display: flex;
+        flex-direction: column;
+        place-items: center;
+        box-sizing: border-box;
+
+        width: 70%;
+        max-width: 960px;
+
+        margin-top: 1rem;
+        margin-bottom: 2rem;
+        padding: 1rem 4rem;
+
+        border-style: solid;
+        border-radius: 6px;
+        border-width: var(--width-section-border);
+        border-color: var(--color-section-border);
+    }
+</style>
