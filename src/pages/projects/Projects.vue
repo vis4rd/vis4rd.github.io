948b00925bfe972b7730cf7bdbe327a106003164
vis4rd <alagaesia.vrael@gmail.com>
2023-08-16 00:55:49 +0200

    Add Monoshot project to the list

--- src/pages/projects/Projects.vue
+++ src/pages/projects/Projects.vue
@@ -3,16 +3,49 @@
 </script>
 
 <template>
-    <ProjectTab>
-        <template #title>My own website</template>
-        <template #date>2023</template>
-        <template #href>
-            <a href="https://github.com/vis4rd/vis4rd.github.io">GitHub</a>
-        </template>
-        <template #languages>Vue 3</template>
-        <template #libraries>Vite, Google Fonts, FontAwesome</template>
-        <template #content><img /></template>
-    </ProjectTab>
+    <div class="projects">
+        <ProjectTab>
+            <template #title>My own website</template>
+            <template #date>2023</template>
+            <template #href>
+                <a href="https://github.com/vis4rd/vis4rd.github.io"
+                    >https://github.com/vis4rd/vis4rd.github.io</a
+                >
+            </template>
+            <template #languages>Vue 3</template>
+            <template #libraries>Vite, Google Fonts, FontAwesome</template>
+            <template #content>
+                <img src="/assets/visard.png" />
+            </template>
+        </ProjectTab>
+
+        <ProjectTab>
+            <template #title>Monoshot game</template>
+            <template #date>2022</template>
+            <template #href>
+                <a href="https://github.com/vis4rd/monoshot/tree/dev"
+                    >https://github.com/vis4rd/monoshot/tree/dev</a
+                >
+            </template>
+            <template #languages>C++, GLSL</template>
+            <template #libraries>OpenGL, GLFW, ImGui, EnTT</template>
+            <template #content>
+                <img src="/assets/monoshot.png" />
+            </template>
+        </ProjectTab>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+    .projects {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        width: 100%;
+        height: 100%;
+
+        margin: 0;
+        padding: 0;
+    }
+</style>
