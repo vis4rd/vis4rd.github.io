2d91229d48b883e87488df688aa5664a3b8f46d5
vis4rd <alagaesia.vrael@gmail.com>
2023-08-16 17:25:55 +0200

    Improve ProjectTab appearance with text instead of images

--- src/pages/projects/Projects.vue
+++ src/pages/projects/Projects.vue
@@ -20,6 +20,23 @@
         </ProjectTab>
 
         <ProjectTab>
+            <template #title>Pydocstyle Gitlab CQ report</template>
+            <template #date>2023</template>
+            <template #href>
+                <a href="https://github.com/vis4rd/pydocstyle-gitlab-code-quality">
+                    https://github.com/vis4rd/pydocstyle-gitlab-code-quality
+                </a>
+            </template>
+            <template #languages>Python 3.7-3.11</template>
+            <template #libraries>Black, isort, mypy, pylint, pydocstyle</template>
+            <template #content>
+                It is a simple script which converts default pydocstyle output into Gitlab Code
+                Quality report. This report displays all errors and warnings in Gitlab UI, in
+                example after Merge Request pipeline is finished.
+            </template>
+        </ProjectTab>
+
+        <ProjectTab>
             <template #title>Monoshot game</template>
             <template #date>2022</template>
             <template #href>
@@ -27,10 +44,25 @@
                     >https://github.com/vis4rd/monoshot/tree/dev</a
                 >
             </template>
-            <template #languages>C++, GLSL</template>
+            <template #languages>C++, GLSL, CMake</template>
             <template #libraries>OpenGL, GLFW, ImGui, EnTT</template>
             <template #content>
                 <img src="/assets/monoshot.png" />
+            </template>
+        </ProjectTab>
+
+        <ProjectTab>
+            <template #title>University labs and projects</template>
+            <template #date>2019 - 2023</template>
+            <template #href>
+                <a href="https://github.com/orgs/Aleksander-Kluczka-AGH/repositories"
+                    >My GitHub organization</a
+                >
+            </template>
+            <template #languages>12+ programming languages</template>
+            <template #libraries>20+ libraries and frameworks</template>
+            <template #content>
+                <img src="/assets/agh-ust.jpg" />
             </template>
         </ProjectTab>
     </div>
