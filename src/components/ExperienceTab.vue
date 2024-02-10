8ae6108bf744b17483413af454b37af7ddba747a
vis4rd <alagaesia.vrael@gmail.com>
2024-02-11 00:13:06 +0100

    Add Experience subpage and ExperienceTab component

---
+++ src/components/ExperienceTab.vue
@@ -0,0 +1,102 @@
+<script setup>
+    import IconInfo from "@/components/IconInfo.vue";
+    import BorderWrapper from "@/components/BorderWrapper.vue";
+</script>
+
+<template>
+    <BorderWrapper
+        padding_left="2rem"
+        padding_right="2rem"
+        padding_top="1rem"
+        padding_bottom="1rem"
+    >
+        <div class="experience_tab">
+            <h1>
+                <slot name="job_title">Job title</slot>
+            </h1>
+            <div class="experience">
+                <div class="short">
+                    <div class="details_list">
+                        <IconInfo icon="person_4" v-if="!!this.$slots.company">
+                            <slot name="company">Company</slot>
+                        </IconInfo>
+                        <IconInfo icon="calendar_month" v-if="!!this.$slots.date">
+                            <slot name="date">
+                                <span style="color: red">
+                                    Remove template tag if you want to remove the whole line with
+                                    icon.
+                                </span>
+                            </slot>
+                        </IconInfo>
+                        <IconInfo icon="code" v-if="!!this.$slots.languages">
+                            <slot name="languages">
+                                <span style="color: red">
+                                    Remove template tag if you want to remove the whole line with
+                                    icon.
+                                </span>
+                            </slot>
+                        </IconInfo>
+                        <IconInfo icon="deployed_code" v-if="!!this.$slots.libraries">
+                            <slot name="libraries">
+                                <span style="color: red">
+                                    Remove template tag if you want to remove the whole line with
+                                    icon.
+                                </span>
+                            </slot>
+                        </IconInfo>
+                        <IconInfo icon="build" v-if="!!this.$slots.tools">
+                            <slot name="tools">
+                                <span style="color: red">
+                                    Remove template tag if you want to remove the whole line with
+                                    icon.
+                                </span>
+                            </slot>
+                        </IconInfo>
+                    </div>
+                </div>
+
+                <div class="description">
+                    <slot name="description">Job description</slot>
+                </div>
+            </div>
+        </div>
+    </BorderWrapper>
+</template>
+
+<style scoped>
+    .experience_tab {
+        display: flex;
+        flex-direction: column;
+
+        width: 100%;
+        font-size: calc(var(--size-text) - 2px);
+    }
+
+    .experience {
+        display: flex;
+
+        width: 100%;
+        height: min-content;
+    }
+
+    .experience > * {
+        width: 100%;
+    }
+
+    .short {
+        /* border: solid 1px red; */
+        width: 40%;
+    }
+
+    .description {
+        padding-left: 1rem;
+        /* border: solid 1px blue; */
+    }
+
+    h1,
+    h2,
+    h3 {
+        margin: 0;
+        padding: 0;
+    }
+</style>
---
+++ src/pages/experience/Experience.vue
@@ -0,0 +1,92 @@
+<script setup>
+    import ExperienceTab from "@/components/ExperienceTab.vue";
+</script>
+
+<template>
+    <div class="experience_list">
+        <ExperienceTab>
+            <template #company>
+                <a href="https://www.nokia.com/networks/" target="_blank">Nokia</a>
+            </template>
+            <template #job_title> Mid Software Engineer </template>
+            <template #date> 2023.07 - present </template>
+            <template #languages> C++17, Python 3.12, TTCN3 </template>
+            <template #tools> CMake, Git, Jira, Gerrit, GitLab, GDB, Docker </template>
+            <template #description>
+                <p>
+                    I got promoted to a mid software engineer, which was a great success for me. I
+                    continued to improve my skills in day-to-day work and expand my knowledge about
+                    how 5G networks work under the hood.
+                </p>
+                <p>
+                    My responsibilites has expanded as well. I got involved in fixing various
+                    problem reports raised by internal testers and customers.
+                </p>
+            </template>
+        </ExperienceTab>
+        <ExperienceTab>
+            <template #company>
+                <a href="https://www.nokia.com/networks/" target="_blank">Nokia</a>
+            </template>
+            <template #job_title> C++ Working Student </template>
+            <template #date> 2022.04 - 2023.06 </template>
+            <template #languages> C++17, Python 3.11, TTCN3 </template>
+            <template #tools> CMake, Git, Jira, Gerrit, GitLab, GDB, Docker </template>
+            <template #description>
+                <p>
+                    I joined the team as a working student and was responsible for developing new
+                    features to 5G network. I cooperated with both other developers and architects.
+                    Every code change had to be covered by unit and component tests.
+                </p>
+                <p>
+                    Additionally I developed an internal tool that was used to assign code reviewers
+                    to merge requests based on arbitrary rules. Here I had much greater input into
+                    application design and architecture.
+                </p>
+                <p>
+                    During this time I learned a lot about how to work in a team, but also about
+                    software development, including high performance and multithreaded applications.
+                </p>
+            </template>
+        </ExperienceTab>
+        <ExperienceTab>
+            <template #company>
+                <a href="https://www.agh.edu.pl/en/" target="_blank">AGH UST</a>
+            </template>
+            <template #job_title> Applied Computer Science Student </template>
+            <template #date> 2019.10 - present </template>
+            <template #description>
+                <p>
+                    I am currently a master's student at AGH UST in Applied Computer Science. Feel
+                    free to check out my journey on
+                    <a href="https://github.com/Aleksander-Kluczka-AGH" target="_blank"
+                        >my GitHub</a
+                    >
+                </p>
+            </template>
+        </ExperienceTab>
+    </div>
+</template>
+
+<style scoped>
+    .experience_list {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        width: 100%;
+        height: 100%;
+
+        margin: 0;
+        padding: 0;
+    }
+
+    .experience_list p {
+        margin: 0;
+        padding: 0;
+        padding-bottom: 1rem;
+    }
+    .experience_list p:last-child {
+        padding: 0;
+    }
+</style>
