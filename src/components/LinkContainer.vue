961a4ce5e27d23d1b5efca3af3d2108c0afd3e8f
vis4rd <alagaesia.vrael@gmail.com>
2024-02-17 20:14:21 +0100

    Add LinkContainer component

---
+++ src/components/LinkContainer.vue
@@ -0,0 +1,64 @@
+<script setup>
+    const props = defineProps({
+        href: {
+            type: String,
+            default: "",
+        },
+        target: {
+            type: String,
+            default: "_self",
+        },
+        title: {
+            type: String,
+            default: "",
+        },
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
+        back_to_top: {
+            type: Boolean,
+            default: false,
+        },
+        local: {
+            type: Boolean,
+            default: false,
+        },
+    });
+
+    function backToTop() {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+</script>
+
+<template>
+    <a
+        v-if="!props.local"
+        class="link_container"
+        :to="props.href"
+        :title="props.title"
+        :disabled="props.disabled"
+        :target="props.target"
+        @click="props.back_to_top ? backToTop() : null">
+        <slot></slot>
+    </a>
+    <router-link v-else class="link_container" :to="props.href" :title="props.title">
+        <slot></slot>
+    </router-link>
+</template>
+
+<style scoped>
+    .link_container {
+        display: flex;
+
+        padding: 1rem;
+        margin: 0;
+
+        border-style: solid;
+        border-color: var(--color-element-border);
+        border-width: 1px;
+        border-radius: 6px;
+
+        cursor: pointer;
+    }
+</style>
