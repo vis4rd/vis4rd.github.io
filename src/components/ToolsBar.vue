1e4c474706c9804a69b4321fdeec15286c6aa1ce
vis4rd <alagaesia.vrael@gmail.com>
2024-02-17 20:14:05 +0100

    Add ToolsBar component

---
+++ src/components/ToolsBar.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+    <div class="tools_row">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+    .tools_row {
+        display: flex;
+        flex-direction: row-reverse;
+
+        justify-content: right;
+        align-items: center;
+
+        gap: 1rem;
+
+        width: 70%;
+        max-width: 960px;
+
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+    }
+</style>
